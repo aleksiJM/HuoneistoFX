4dc767812ba7a19a72da145be608848395ff2fd2
AleksiJim <metsaal@gmail.com>
2026-04-06 23:41:29 +0300

    Tapahtumankäsittelijät (10.1)

--- src/main/java/fi/jyu/ohj2/aleksi/huoneisto/MainController.java
+++ src/main/java/fi/jyu/ohj2/aleksi/huoneisto/MainController.java
@@ -1,12 +1,52 @@
 package fi.jyu.ohj2.aleksi.huoneisto;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
+    public TextField asukkaanNimi;
+    public TextField asukkaanIka;
+    public TextField asukkaanYhteystiedot;
+    public TextField asukkaidenMaara;
+    public TextField asunnonTunnus;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         // Write initialization code here
+    }
+
+    public void lisaaAsukas(ActionEvent actionEvent) {
+        
+    }
+
+    public void vahvistaAsukas(ActionEvent actionEvent) {
+    }
+
+    public void suljeAsukas(ActionEvent actionEvent) {
+    }
+
+    public void vahvistaAsunto(ActionEvent actionEvent) {
+    }
+
+    public void suljeAsunto(ActionEvent actionEvent) {
+    }
+
+    public void lisaaAsunto(ActionEvent actionEvent) {
+    }
+
+    public void muokkaaAsuntoa(ActionEvent actionEvent) {
+    }
+
+    public void poistaAsunto(ActionEvent actionEvent) {
+    }
+
+    public void poistaAsukas(ActionEvent actionEvent) {
+    }
+
+    public void suljeMuokkaus(ActionEvent actionEvent) {
     }
 }
